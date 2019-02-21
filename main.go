f529cb365ee9c7dd03b1d1a48c71f0998c3289b6
Hoon <hoon.kong@pluralstack.io>
2019-02-21 02:24:51 -0800

    Basic template binding

--- main.go
+++ main.go
@@ -3,17 +3,35 @@ package main
 import (
     "fmt"
     "net/http"
+    "html/template"
 )
+
+// Item struct
+type Item struct {
+    Name    string
+    Price   float64
+}
 
 func headers(w http.ResponseWriter, r *http.Request) {
     h := r.Header
     fmt.Fprintln(w, h)
 }
 
-func main() {
-    server := http.Server {
-        Addr: "127.0.0.1:8080",
+func home(w http.ResponseWriter, r *http.Request) {
+    someData := []Item {
+        Item { "Apple", 2.99 },
+        Item { "Pear", 3.99 },
+        Item { "Orange", 4.99 },
     }
-    http.HandleFunc("/headers", headers)
-    server.ListenAndServe();
+
+    templates := template.Must(template.ParseGlob("templates/*.html"))
+    templates.ExecuteTemplate(w, "master", someData)
+}
+
+func main() {
+     files := http.FileServer(http.Dir("static"))
+    http.Handle("/static/", http.StripPrefix("/static/", files))
+
+    http.HandleFunc("/home", home)
+    http.ListenAndServe(":8080", nil)
 }
