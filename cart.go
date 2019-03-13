60f2b271a519352d7251370c0dd00b995bdf6a63
Hoon <hoon.kong@pluralstack.io>
2019-03-13 01:39:38 -0700

    Format the total

--- cart.go
+++ cart.go
@@ -3,13 +3,16 @@ package main
 import (
     "net/http"
     "strconv"
+    "math"
+    "fmt"
 )
 
 type cartItem struct {
-    ID          string
-    UnitItem    item
-    Quantity    int
-    Total       float64
+    ID              string
+    UnitItem        item
+    Quantity        int
+    Total           float64
+    FormattedTotal  string
 }
 
 var cart = make(map[string]*cartItem)
@@ -22,7 +25,7 @@ func addItem(w http.ResponseWriter, r *http.Request) {
     itemID := r.FormValue("id")
     quantity, _ := strconv.ParseFloat(r.FormValue(itemID), 64)
     unitPrice, _ := strconv.ParseFloat(r.FormValue("price"), 64)
-    totalPrice := unitPrice * quantity
+    totalPrice := math.Ceil((unitPrice * quantity) * 100) / 100
 
     if cart[itemID] == nil {
         cart[itemID] = &cartItem {
@@ -30,10 +33,12 @@ func addItem(w http.ResponseWriter, r *http.Request) {
             UnitItem: allItems[itemID],
             Quantity: int(quantity),
             Total: totalPrice,
+            FormattedTotal: fmt.Sprintf("%.2f", totalPrice),
         }
     } else {
         cart[itemID].Quantity += int(quantity)
         cart[itemID].Total += totalPrice
+        cart[itemID].FormattedTotal = fmt.Sprintf("%.2f", cart[itemID].Total)
     }
 
     renderPage(w, "templates/cart.html", cart)
